760d365b614f03e8d77ef7d3896f670c1a0170eb
dostonbek <dostonbek>
2025-09-22 06:02:33 +0500

    5

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ elBtn.addEventListener("click", () => {
   }
 });
 // localstorage
-let cars = JSON.parse(localStorage.getItem("cars_data"));
+let cars = JSON.parse(localStorage.getItem("cars_data") || initialCars);
 let tahrirlashId = null;
 let batafsilId = null;
 
